cbde7e5e60b41f0b7598c6c61726e1fe166a5c4c
anthony-malerba <anthony.malerba@he-arc.ch>
2015-05-10 12:57:30 +0200

    Commit afficage

--- src/ch/hearc/ig/sectec/serie3/adfgvx/business/UI.java
+++ src/ch/hearc/ig/sectec/serie3/adfgvx/business/UI.java
@@ -1,5 +1,9 @@
 package ch.hearc.ig.sectec.serie3.adfgvx.business;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map.Entry;
+
 public class UI {
 
 	/**
@@ -7,8 +11,33 @@ public class UI {
 	 * @param table
 	 */
 	public void AfficheTable(String[][] table) {
-		// TODO - implement UI.AfficheTable
 		throw new UnsupportedOperationException();
+	}
+	
+	public void AfficheSubstTable(HashMap<String, String[]> substTable) {
+            
+            StringBuilder sb = new StringBuilder(); 
+            
+            for (Entry<String, String[]> coupleLettres : substTable.entrySet()) {
+                sb.append(coupleLettres.getKey()).append(" ");
+                sb.append(coupleLettres.getValue()[0]+ coupleLettres.getValue()[1]); 
+                sb.append("\n");
+            }
+            
+            System.out.println(sb.toString()); 
+
+            
+            
+            /*
+            Iterator iterator = substTable.keySet().iterator();  
+
+            System.out.println("coufcouss"); 
+                while(iterator.hasNext()){
+                    String key = (String) iterator.next();
+
+                    System.out.println("UI : Coucou "); 
+                    //System.out.println(substTable.get(key)[0].toString() + substTable.get(key)[1].toString()); 
+                }*/
 	}
 
 }
